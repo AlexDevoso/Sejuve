41a6976109b35a106b07d41bbc6b0745cb89256d
AlexLlima <36746185+AlexLlima@users.noreply.github.com>
2020-01-24 08:15:26 -0300

    Delete teste.php

--- teste.php
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-              var categoria1 = "oi";
-              var naipe1 = document.getElementById("naipe").value;
-            </script>
-            <?php 
-    $ct =  "<script>document.write(categoria1)</script>";
-    echo $ct;   
-  ?>
