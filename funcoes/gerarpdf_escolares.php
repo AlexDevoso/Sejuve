03e0ebbf5bd5c24e1e2d98f3c83052f5dd257c58
AlexLlima <36746185+AlexLlima@users.noreply.github.com>
2020-02-04 16:32:41 -0300

    Add files via upload

--- funcoes/gerarpdf_escolares.php
+++ funcoes/gerarpdf_escolares.php
@@ -1,17 +1,20 @@
 <?php
-			include "conexao.php";
-			session_start();
-			$verifica = $_SESSION['login_escola'][0];
-  			$seleciona = "SELECT escola_id from escola where login = '$verifica'";
-  			$resus = mysqli_query($conexao, $seleciona);
-  			$verificaid = mysqli_fetch_row($resus);
-  			$str = implode($verificaid);
-			//mostrar id do evento
-			$seleciona2 = "SELECT modalidade_coletivaid from modalidade_coletiva where nome_mc = '$nome_modalidade'";
-  			$resus2 = mysqli_query($conexao, $seleciona2);
-  			$verificaid2 = mysqli_fetch_row($resus2);
-			$str2 = implode($verificaid2);
-				//acaba aqui  
+	include "conexao.php";
+	session_start();
+	if (isset($_GET['modalidade_coletivaid'])) {
+		$modalidade_coletivaid = $_GET['modalidade_coletivaid'];
+}
+	$verifica = $_SESSION['login_escola'][0];
+  	$seleciona = "SELECT escola_id from escola where login = '$verifica'";
+  	$resus = mysqli_query($conexao, $seleciona);
+  	$verificaid = mysqli_fetch_row($resus);
+  	$str = implode($verificaid);
+	//mostrar id do evento
+	$seleciona2 = "SELECT modalidade_coletivaid from modalidade_coletiva where modalidade_coletivaid = '$modalidade_coletivaid'";
+  	$resus2 = mysqli_query($conexao, $seleciona2);
+  	$verificaid2 = mysqli_fetch_row($resus2);
+	$str2 = implode($verificaid2);
+	//acaba aqui  
 
 			
 
@@ -23,7 +26,7 @@
 	require_once  '../dompdf/autoload.inc.php'; 
 	
 
-	$sql = "SELECT * FROM aluno where escola_id_fk = '$str'";
+	$sql = "SELECT * FROM aluno where escola_id_fk = '$str' and modalidade_coletivaid_fk = '$modalidade_coletivaid'";
 	$result = mysqli_query($conexao, $sql);
 	$sqlescola = "SELECT * from escola where escola_id = '$str'";
 	$resultescola = mysqli_query($conexao, $sqlescola);
@@ -37,12 +40,11 @@
             </div> ';
             
 	$html .= '<img src="../imagens/'.$algumacoisa.'" style="width:100px; height:100px;" id="imglogo3">';
-	while ($dadosequipe = mysqli_fetch_assoc($resultequipe)) {
-		$html .='<h2 align="center" > EQUIPE: '.$dadosequipe['nomeequipe'].'</h2>';
-		$html .='<h3 align="center"> RESPONSAVEL: '.$dadosequipe['nomeresponsavel'].'</h3>';
-	}
+		$html .='<h2 align="center" > ESCOLA: '.$dadosescola['nomeequipe'].'</h2>';
+		$html .='<h3 align="center"> DIRETOR: '.$dadosescola['nomeresponsavel'].'</h3>';
+		$html .='<h3 align="center"> INEP: '.$dadosescola['nomeresponsavel'].'</h3>';		
 	
-	$html .= '<h2 align="center">Atletas<h2>';
+	$html .= '<h2 align="center">Alunos<h2>';
 	$html .= '
 	<style>
 
@@ -78,46 +80,23 @@
 	$html .= '<table border = 1 width = 100%>';
 	$html .= '<thead>';
 		$html .= '<tr align="center">';
-			$html .= '<td>Nome do Atleta</td>';
+			$html .= '<td>Nome do Aluno</td>';
 			$html .= '<td>Data de Nascimento</td>';
-			$html .= '<td>Rg do Atleta</td>';
-			$html .= '<td>Naturalidade do Atleta</td>';			
+			$html .= '<td>Rg do Aluno</td>';
 		$html .= '</tr>';
 	$html .= '</thead>';
 	$html .='<tbody>';
 	while ($linha = mysqli_fetch_assoc($result)) {
-		$html .='<tr align="center"><td>'.$linha['nome_atleta'].'</td>';
-		$html .='<td>'.$linha['data_nascimento_atleta'].'</td>';
-		$html .='<td>'.$linha['rg_atleta'].'</td>';
-		$html .='<td>'.$linha['naturalidade_atleta'].'</td></tr>';
+		$html .='<tr align="center"><td>'.$linha[''].'</td>';
+		$html .='<td>'.$linha['nome'].'</td>';
+		$html .='<td>'.$linha['data_nascimento_aluno'].'</td>';
+		$html .='<td>'.$linha['rg'].'</td></tr>';
 		$html .='</tbody>';
 	}
 
-	
-	$html .='</table>';
-
-	$html .= '<h2 align="center">Comissão Tecnica<h2>';
-	$html .= '<table border = 1 width = 100%>';
-	$html .= '<thead>';
-		$html .= '<tr align="center">';
-			$html .= '<td>Nome</td>';
-			$html .= '<td>Função</td>';			
-		$html .= '</tr>';
-	$html .= '</thead>';
-	$html .='<tbody>';
-	while ($linha2 = mysqli_fetch_assoc($result2)) {
-		
-		$html .='<tr align="center" ><td>'.$linha2['nome'].'</td>';
-		$html .='<td>'.$linha2['funcao'].'</td></tr>';
-
-		$html .='</tbody>';
-	}
-
-	
 	$html .='</table>';
 	
 	
-
 
 	//instancia
 	$dompdf = new Dompdf;
