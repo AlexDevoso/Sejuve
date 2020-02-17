b8bf6b118dfe56d45e8f796fc09b499ac8029ea5
AlexLlima <36746185+AlexLlima@users.noreply.github.com>
2020-02-17 16:30:26 -0300

    Add files via upload

--- interface/cadastrar_aluno_jogos.php
+++ interface/cadastrar_aluno_jogos.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include "../funcoes/conexao.php";
+include "../funcoes/mensagem.php";
 
 
 
@@ -81,6 +82,12 @@ if($_SESSION['modalidade'][2] == "coletiva"){
         <link rel="shortcut icon" type="image/x-icon" href="../imagens/sistema/fav_icon/fav_icon.ico">
 
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400" rel="stylesheet">
+
+        <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
 <style type="text/css">
   
@@ -491,6 +498,7 @@ customTxt.innerHTML ="Nenhum arquivo selecionado.";
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  
 
   <!-- Menu Toggle Script -->
   <script>
